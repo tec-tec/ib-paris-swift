e2f54d4b08bebfa4c70a6db1fe2468758725541c
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2016-06-09 17:53:16 +0200

    Added notifications to update views

---
+++ Restos/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Restos
+//
+//  Created by Ludovic Ollagnier on 09/06/2016.
+//  Copyright © 2016 Tec-Tec. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+
+    struct Notifications {
+        static let modelUpdatedNotifName = "ModelUpdated"
+    }
+}
